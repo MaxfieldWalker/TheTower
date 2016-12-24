4e0766de8ff17b70300d803984f036162a126aba
Maxfield Walker <maxfield.walker@gmail.com>
2016-12-24 22:17:55 +0900

    add TheTower project and create title scene

---
+++ TheTower/Assets/GameMode.cs
@@ -0,0 +1,19 @@
+﻿namespace Assets
+{
+    public enum GamePlayerMode
+    {
+        OnePlayer, // ひとりでプレイ
+        TwoPlayers, // ふたりでプレイ
+
+        Unknown
+    }
+
+    public enum GameLevel
+    {
+        Level1, // 初級
+        Level2, // 中級
+        Level3, // 上級
+
+        Unknwon
+    }
+}
